28ad073378fa0324a74ac8e022cbdba47a8324bb
Chandler May <cjmay4754@gmail.com>
2017-06-19 18:19:01 -0400

    add inotifywait example

---
+++ push-written.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 inotifywait-arg [inotifywait-arg ...]" >&2
+    echo "Examples:" >&2
+    echo "  $0 -r foo/bar" >&2
+    echo "  $0 -r foo/bar/baz" >&2
+    exit 1
+fi
+
+set -e
+
+inotifywait --format '%w%f' -m -e close_write "$@" 2>/dev/null | \
+    while read path
+    do
+        gdcp.py push $path
+    done
